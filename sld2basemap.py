5cf0012b72b35658f57d9acf39632ab83085152f
KADASTER-NL\kostet <tom.kostense@kadaster.nl>
2019-02-14 13:17:42 +0100

    upgrade python 2.7 -> python 3.6

--- sld2basemap.py
+++ sld2basemap.py
@@ -69,7 +69,7 @@ def executeXslt(xsl_filename, xml_filename):
 
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
-		print "Syntax: %s <CSV-file> <SLD-dir> [<Title>]" % sys.argv[0]
+		print("Syntax: %s <CSV-file> <SLD-dir> [<Title>]" % sys.argv[0])
 		exit(1)
 		
 	csvFile = sys.argv[1]
@@ -79,30 +79,12 @@ if __name__ == "__main__":
 	serviceTitle = sys.argv[3] if len(sys.argv) > 3 else baseName
 		
 	if not os.path.isdir(sldDir):
-		print "SLD dir does not exist!"
+		print("SLD dir does not exist!")
 		exit(1)
 		
 	if not os.path.exists(csvFile):
-		print "CSV file does not exist!"
+		print("CSV file does not exist!")
 		exit(1)
-		
-	#print 'MAP'
-	#print '  NAME          "%s"' % baseName
-	#print '  INCLUDE       "header.inc"'
-	#print ''
-	#print '  WEB'
-	#print '    METADATA'
-	#print '      "ows_title"               "%s"' % serviceTitle
-	#print '      "ows_abstract"            "%s"' % serviceTitle
-	#print '      "ows_keywordlist"         "PDOK Geopackage Mapserver %s"' % serviceTitle
-	#print '      # The disadvantage of setting an onlineresource URL is that the servername is fixed. It can\'t'
-	#print '      # be changed through an environment variable, or by modifying the Apache config.'
-	#print '      #"ows_onlineresource"      "http://localhost/maps/example?"'
-	#print '      "wfs_namespace_prefix"    "%s"' % baseName
-	#print '      "wfs_namespace_uri"       "http://example.org/pdok/%s"' % baseName
-	#print '    END'
-	#print '  END'
-	#print ''
 		
 	with open(csvFile, 'r') as f:
 	
@@ -126,96 +108,96 @@ if __name__ == "__main__":
 				for style in styleList:
 					result = executeXslt('sld2map.xslt', '%s/%s.sld' % (sldDir, style))
 					classesList.append(result)
-			except subprocess.CalledProcessError, e:
-				print "Ping stdout output:\n", e.output
+			except subprocess.CalledProcessError as e:
+				print("Ping stdout output:\n", e.output)
 				exit(1)
 				
 			scaleDenoms = getMinMaxScaleDenoms(classesList)
 			
-			print 'LAYER'
-			print '  NAME "%s"' % layer
-			print '  STATUS ON'
-			print '  LABELCACHE ON'
+			print('LAYER')
+			print('  NAME "%s"' % layer)
+			print('  STATUS ON')
+			print('  LABELCACHE ON')
 
 			if geomtype == 'line' or 'lijn' in layer.lower():
-				print '  TYPE LINE'
+				print('  TYPE LINE')
 			elif geomtype == 'area' or 'vlak' in layer.lower():
-				print '  TYPE POLYGON'
+				print('  TYPE POLYGON')
 			elif geomtype == 'label' in layer.lower():
-				print '  TYPE POINT'
+				print('  TYPE POINT')
 			else:
-				print '  TYPE POINT'
+				print('  TYPE POINT')
 				
 			# TODO: allow custom extent?
-			print '  EXTENT 0 200000 280000 725000'
-			print ''
+			print('  EXTENT 0 200000 280000 725000')
+			print('')
 			
-			print '  METADATA'
-			print '    "ows_title"               "%s"' % layer
-			print '    "wms_include_items"       "sld_all"'
-			print '    "gml_include_items"       "sld_all"'
-			print '    "gml_featureid"           "gid"'
-			print '    "gml_geometries"          "geom"'
-			print '    "ows_metadataurl_type"    "TC211"'
-			print '    "ows_metadataurl_format"  "text/plain"'
-			print '    "ows_metadataurl_href"    "http://nationaalgeoregister.nl/geonetwork/srv/dut/xml.metadata.get?uuid=123"'
-			print '    "ows_group_title"         "%s"' % baseName
-			print '  END'
-			print ''
-			print '  CONNECTIONTYPE OGR'
-			print '  CONNECTION "/srv/data/%s"' % geopackage
+			print('  METADATA')
+			print('    "ows_title"               "%s"' % layer)
+			print('    "wms_include_items"       "sld_all"')
+			print('    "gml_include_items"       "sld_all"')
+			print('    "gml_featureid"           "gid"')
+			print('    "gml_geometries"          "geom"')
+			print('    "ows_metadataurl_type"    "TC211"')
+			print('    "ows_metadataurl_format"  "text/plain"')
+			print('    "ows_metadataurl_href"    "http://nationaalgeoregister.nl/geonetwork/srv/dut/xml.metadata.get?uuid=123"')
+			print('    "ows_group_title"         "%s"' % baseName)
+			print('  END')
+			print('')
+			print('  CONNECTIONTYPE OGR')
+			print('  CONNECTION "/srv/data/%s"' % geopackage)
 			
 			# TODO: misschien altijd alles nodig i.g.v. GetFeatureInfo / WFS.
 			# Splitsing services voor visualisatie en bevragen mogelijk?
 			if len(classesList) > 0 or 'LABEL' in classesList[0]:
 				# Alle velden nodig, het te querien veld moet nog uit 'classes' worden geparsed.
 				# Bij meerdere stijlen is vooralsnog teveel werk om de benodigde velden te parsen.
-				print '  DATA "select * from %s"' % layer
+				print('  DATA "select * from %s"' % layer)
 			else:
 				classitem = getClassitem(classesList[0])
 				if classitem is not None:
-					print '  DATA "select geom, %s from %s"' % (classitem, layer)
+					print('  DATA "select geom, %s from %s"' % (classitem, layer))
 				else:
-					print '  DATA "select geom from %s"' % layer
-			print '  PROCESSING "CLOSE_CONNECTION=DEFER"'
+					print('  DATA "select geom from %s"' % layer)
+			print('  PROCESSING "CLOSE_CONNECTION=DEFER"')
 			
 			if 'min' in scaleDenoms and scaleDenoms['min'] is not None:
-				print '  MINSCALEDENOM %.f' % scaleDenoms['min']
+				print('  MINSCALEDENOM %.f' % scaleDenoms['min'])
 			if 'max' in scaleDenoms and scaleDenoms['max'] is not None:
-				print '  MAXSCALEDENOM %.f' % scaleDenoms['max']
-			print '  TEMPLATE "dummy"'
-			print ''
+				print('  MAXSCALEDENOM %.f' % scaleDenoms['max'])
+			print('  TEMPLATE "dummy"')
+			print('')
 			
 			if False:
 				# Basisstijl
-				print '  CLASS'
-				print '    STYLE'
+				print('  CLASS')
+				print('    STYLE')
 				if 'lijn' in layer.lower():
-					print '      COLOR "#000000"'
+					print('      COLOR "#000000"')
 				elif 'vlak' in layer.lower():
-					print '      COLOR "#808080"'
-					print '      OUTLINECOLOR "#000000"'
+					print('      COLOR "#808080"')
+					print('      OUTLINECOLOR "#000000"')
 				else:
-					print '      COLOR "#000000"'
-				print ''
-				
-				print '    END'
-				print '  END'
+					print('      COLOR "#000000"')
+				print('')
+
+				print('    END')
+				print('  END')
 			else:
 				# Vanuit SLD omgezette stijlen
-				print '  CLASSGROUP "%s"' % styleList[0]
+				print('  CLASSGROUP "%s"' % styleList[0])
 
 				for i in range(0, len(styleList)):
-					print classesList[i].replace('{group-placeholder}', styleList[i])
+					print(classesList[i].replace('{group-placeholder}', styleList[i]))
 			
-			print 'END # LAYER'
-			print ''
+			print('END # LAYER')
+			print('')
 			
 			for style in styleList:
 				namedStyles = executeXslt('sld2namedStyles.xslt', '%s/%s.sld' % (sldDir, style))
 				
 				if len(namedStyles) > 0:
-					print namedStyles.strip('\n')
-					print ''
+					print(namedStyles.strip('\n'))
+					print('')
 				
-	#print 'END # MAP'
+	#print('END # MAP'
