128edc31c259a8bc5e720ad9ce919eb2f1965c97
Arno <arno.timmer@kadaster.nl>
2022-06-02 14:13:02 +0200

    fix tests

--- test/test_util.py
+++ test/test_util.py
@@ -5,7 +5,7 @@ class TestUtil(unittest.TestCase):
 
     def test_get_output_filepath(self):
         output_path = _get_output_filepath("test/sld/version/1.1.0.sld", "output_dir", ".style")
-        self.assertEqual(output_path, "output_dir/1.1.0.style")
+        self.assertEqual(output_path, r"output_dir\1.1.0.style")
 
 if __name__ == '__main__':
     unittest.main()
